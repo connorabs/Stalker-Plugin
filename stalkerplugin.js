14002f24526a5c57fc124b3bd6de9f19f9a07cfb
cnr <conwilkerson42@gmail.com>
2025-02-15 12:49:40 -0600

    Update stalkerplugin.js

--- stalkerplugin.js
+++ stalkerplugin.js
@@ -1,43 +1,23 @@
 // ==UserScript==
 // @name         Stalker Ploogin
-// @version      0.1
+// @version      0.2
 // @namespace    http://tampermonkey.net/
-// @description  fucking retards
+// @description  Block the new PolarisStoriesV3SeenMutation.graphql requests
 // @author       CNR
 // @match        https://www.instagram.com/*
 // @grant        none
 // ==/UserScript==
 
+// i figured you could just block the send only and block anything relating to viewseenat since its a string
+// so it is greatly optomizied compared the old script
+
 (function() {
-    'use strict';
-    const originalFetch = window.fetch;
-    window.fetch = function(input, init) {
-        let url = "";
-        if (typeof input === "string") {
-            url = input;
-        } else if (input && typeof input.url === "string") {
-            url = input.url;
+    var penissend = XMLHttpRequest.prototype.send;
+    XMLHttpRequest.prototype.send = function() {
+        if (typeof arguments[0] === "string" && arguments[0].includes("viewSeenAt")) {
+        } else {
+            penissend.apply(this, arguments);
         }
-        if (url.includes("PolarisStoriesV3SeenMutation.graphql")) {
-            console.log("Blocked fetch request to:", url);
-            return Promise.resolve(new Response(null, {status: 204, statusText: "No Content"}));
-        }
-        return originalFetch.apply(this, arguments);
-    };
-
-    const originalOpen = XMLHttpRequest.prototype.open;
-    XMLHttpRequest.prototype.open = function(method, url, async, user, password) {
-        this._url = url;
-        return originalOpen.call(this, method, url, async, user, password);
-    };
-
-    const originalSend = XMLHttpRequest.prototype.send;
-    XMLHttpRequest.prototype.send = function(body) {
-        if (this._url && this._url.includes("PolarisStoriesV3SeenMutation.graphql")) {
-            console.log("Blocked XMLHttpRequest to:", this._url);
-            this.abort();
-            return;
-        }
-        return originalSend.call(this, body);
     };
 })();
+
